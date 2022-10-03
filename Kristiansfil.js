59c63d0067572954cb094a9ba8481b91eb3fb1ef
Kristian1s <114610052+Kristian1s@users.noreply.github.com>
2022-10-03 12:45:51 +0200

    Add files via upload
    Added html
    Added js
    Added css

---
+++ Kristiansfil.js
@@ -0,0 +1,49 @@
+$(function () {
+  $("#velkommen").hover(function () {
+    $("#velkommen").hover(bigger());
+  });
+});
+
+function bigger() {
+  $("#velkommen").toggleClass("size");
+}
+
+$(function () {
+  $("#header2").click(function () {
+    $("#header2").fadeTo("click", 0.2);
+  });
+  $("#header2").hover(function () {
+    $("#header2").fadeTo("mouseleave", 1);
+  });
+});
+
+function b1() {
+  $("p").css("border", "3px solid blue");
+}
+
+$("document").ready(function () {
+  $("#div2 p:odd").css("background-color", "purple");
+});
+
+function b2() {
+  $("p").css("border", "none");
+}
+
+function animate1() {
+  $("#animate11").ready(function () {
+    $("#div2").animate(
+      {
+        left: "+=500",
+        fontSize: "35px",
+        color: "red",
+      },
+      3000,
+      "swing"
+    );
+  });
+}
+function byeButton(){
+    $(this).click(function(){
+        $("button").slideUp("slow");
+    })
+}
